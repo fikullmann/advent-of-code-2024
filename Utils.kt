6bb22419938f84ed6bec54941ab286c53909aa2c
Fiona Kullmann <fiona.kullmann@tu-dortmund.de>
2023-12-20 16:52:07 +0100

    Utils

--- Utils.kt
+++ Utils.kt
@@ -2,3 +2,21 @@ operator fun Pair<Int, Int>.plus(o: Pair<Int, Int>) = (this.first + o.first) to 
 operator fun Pair<Int, Int>.minus(o: Pair<Int, Int>) = (this.first + o.first) to (this.second + o.second)
 operator fun Pair<Int, Int>.times(o: Int) = o *first to o * second
 operator fun Int.times(o: Pair<Int, Int>) = this *o.first to this * o.second
+
+
+// this part is from stackoverflow OWO help
+fun gcd(input: List<Long>): Long {
+    var result = input[0]
+    for (i in 1 until input.size) result = gcd(result, input[i])
+    return result
+}
+
+fun lcm(a: Long, b: Long): Long {
+    return a * (b / gcd(a, b))
+}
+
+fun lcm(input: List<Long>): Long {
+    var result = input[0]
+    for (i in 1 until input.size) result = lcm(result, input[i])
+    return result
+}
