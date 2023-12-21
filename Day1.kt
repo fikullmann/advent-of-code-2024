329b1c81dca6beefd43f87ead25dfe82dad261f1
Fiona Kullmann <fiona.kullmann@tu-dortmund.de>
2023-12-21 22:24:04 +0100

    Day1 change

--- Day1.kt
+++ Day1.kt
@@ -1,32 +1,34 @@
 import java.io.File
-import kotlin.math.min
 
 fun main() {
-    var total = 0
-    File("input/1").forEachLine {
-        var value = ""
-        for(i in it.indices){
-            value += it[i].digitToIntOrNull() ?: matchDigit(it.substring(i..min(it.length -1, i+4)))
-        }
-        total += (value[0] +"" + value[value.length - 1]).toInt()
-    }
-    println(total)
+    Day1.part2().let(::println)
 }
 
-fun matchDigit(string: String): String {
-    var adjLine = string
-    if (adjLine.length == 4) adjLine += "."
-    if (adjLine.length == 3) adjLine += ".."
-    return when {
-        adjLine.matches(Regex("one..")) -> "1"
-        adjLine.matches(Regex("two..")) -> "2"
-        adjLine.matches(Regex("three")) -> "3"
-        adjLine.matches(Regex("four.")) -> "4"
-        adjLine.matches(Regex("five.")) -> "5"
-        adjLine.matches(Regex("six..")) -> "6"
-        adjLine.matches(Regex("seven")) -> "7"
-        adjLine.matches(Regex("eight")) -> "8"
-        adjLine.matches(Regex("nine.")) -> "9"
-        else -> ""
+object Day1 : Day<Int, Int>() {
+    private val input = File("input/1").readLines()
+    private val numberMap = mapOf(
+            "one" to 1,
+            "two" to 2,
+            "three" to 3,
+            "four" to 4,
+            "five" to 5,
+            "six" to 6,
+            "seven" to 7,
+            "eight" to 8,
+            "nine" to 9,
+    )
+
+    override fun part1(): Int = input.sumOf { str ->
+        10 * str.first { it.isDigit() }.digitToInt() + str.last { it.isDigit() }.digitToInt()
+    }
+
+    override fun part2(): Int = input.sumOf { str ->
+        val fNr = str.indexOfFirst { it.isDigit() }
+        val lNr = str.indexOfLast { it.isDigit() }
+        val fText = str.findAnyOf(numberMap.keys) ?: (Int.MAX_VALUE to "")
+        val lText = str.findLastAnyOf(numberMap.keys) ?: (Int.MIN_VALUE to "")
+
+        (10 * if (fNr < fText.first) str[fNr].digitToInt() else numberMap[fText.second]!!) +
+                if (lNr > lText.first) str[lNr].digitToInt() else numberMap[lText.second]!!
     }
 }
